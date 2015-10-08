6c208e23de46754c8defd96d4fae91dc5138f20b
S1507096 <S1507096@8fe30038-93e8-d64f-a5c7-3f0df5b1f493>
2015-10-08 08:53:32 +0000

    仪表盘模板完成

--- Honoka/Source/Honoka/src/main/java/com/mbp/webController/MainRouter.java
+++ Honoka/Source/Honoka/src/main/java/com/mbp/webController/MainRouter.java
@@ -5,13 +5,24 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
-@RequestMapping("/")
+
 public class MainRouter {
-	@RequestMapping(method = RequestMethod.GET)
-	public String indexRouter(ModelMap model) {
+    //首页
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexRouter(ModelMap model) {
         System.out.println("In Index");
-		model.addAttribute("message", "Hello world!");
-		return "main";
-	}
+        model.addAttribute("message", "Hello world!");
+        return "main";
+    }
+
+    //仪表盘
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public String dashboardRouter(ModelMap model) {
+        System.out.println("In Dashboard");
+        return "dashboard/dashMain";
+    }
+
+
 }
