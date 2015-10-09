d2aa10f8cb63947892910868662040e1955718a5
S1507096 <S1507096@8fe30038-93e8-d64f-a5c7-3f0df5b1f493>
2015-10-09 06:57:54 +0000

    APIKey Service

--- Honoka/Source/Honoka/src/com/honoka/service/APIKeyService.java
+++ Honoka/Source/Honoka/src/com/honoka/service/APIKeyService.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface APIKeyService {
 	public String selectUsableAPIKeyByProvider(String provider);
+	
+	public Integer selectAmountByAPIKey(String key);
 }
