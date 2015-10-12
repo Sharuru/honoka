67bb854a14a8d1cb52ab72659eb230192eaef516
S1507096 <S1507096@8fe30038-93e8-d64f-a5c7-3f0df5b1f493>
2015-10-12 03:07:13 +0000

    转义公司 ID

---
+++ Honoka/Source/Honoka/src/com/honoka/DAO/CompanyMapper.java
@@ -0,0 +1,10 @@
+package com.honoka.DAO;
+
+import java.util.List;
+
+import com.honoka.entity.Company;
+
+public interface CompanyMapper {
+
+	List<Company> getCompanyMap();
+}
---
+++ Honoka/Source/Honoka/src/com/honoka/entity/Company.java
@@ -0,0 +1,41 @@
+package com.honoka.entity;
+
+public class Company {
+	private Integer recordId;
+	private String comId;
+	private String comName;
+	private String comAddr;
+
+	public Integer getRecordId() {
+		return recordId;
+	}
+
+	public void setRecordId(Integer recordId) {
+		this.recordId = recordId;
+	}
+
+	public String getComId() {
+		return comId;
+	}
+
+	public void setComId(String comId) {
+		this.comId = comId;
+	}
+
+	public String getComName() {
+		return comName;
+	}
+
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+
+	public String getComAddr() {
+		return comAddr;
+	}
+
+	public void setComAddr(String comAddr) {
+		this.comAddr = comAddr;
+	}
+
+}
